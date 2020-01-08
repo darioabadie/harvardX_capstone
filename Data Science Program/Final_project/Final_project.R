585fd3986d2e2a0eea723980a8aa80a668b51f19
dario <darioabadie@hotmail.com>
2020-01-07 23:49:49 -0300

    Creation of report

--- Data Science Program/Final_project/Final_project.R
+++ Data Science Program/Final_project/Final_project.R
@@ -108,54 +108,41 @@ train_y <- seeds_y[-test_index]
 train_df <- data.frame(train_x )
 train_df["y"] <- train_y
 
-# Logistic regression
-#fit <- train(y ~ ., method = "glm", data = train_df)
-#glm_preds <- predict(fit,test_x)
+
 
 # LDA
 fit <- train(y ~ ., method = "lda", data = train_df)
 lda_preds <- predict(fit,test_x)
 levels(lda_preds) <- c(3,1,2)
-
+mean(lda_preds == test_y)
 
 # QDA
 fit <- train(y ~ ., method = "qda", data = train_df)
 qda_preds <- predict(fit,test_x)
 levels(qda_preds) <- c(3,1,2)
+mean(qda_preds == test_y)
 
 
 # LOESS
 fit <- train(y ~ ., method = 'gamLoess', data = train_df)
 loess_preds <- predict(fit,test_x)
 levels(loess_preds) <- c(3,1,2)
+mean(loess_preds == test_y)
 
 
 # Random Forest
 fit <- train(y ~ ., method = 'rf', data = train_df,metric = "Accuracy",tuneGrid = expand.grid(.mtry=c(3,5,7,9)))
 rf_preds <- predict(fit,test_x)
 levels(rf_preds) <- c(3,1,2)
+mean(rf_preds == test_y)
 
 
 # K nearest neighbours
 fit <- train(y ~ ., data = train_df, method = "knn", tuneLength = seq(3,21,2))
 knn_preds <- predict(fit,test_x)
 levels(knn_preds) <- c(3,1,2)
+mean(knn_preds == test_y)
 
-
-# K-means
-fit <- kmeans(train_x , 3, iter.max = 10)
-
-predict_kmeans <- function(x, k) {
-  centers <- k$centers    # extract cluster centers
-  # calculate distance to cluster centers
-  distances <- sapply(1:nrow(x), function(i){
-    apply(centers, 1, function(y) dist(rbind(x[i,], y)))
-  })
-  max.col(-t(distances))  # select cluster with min distance to center
-}
-
-kmeans_preds <- predict_kmeans(test_x,fit)
-#levels(kmeans_preds) <- c(1,2,3)
 
 ensemble <- cbind(lda =as.numeric(as.character(lda_preds)), qda =  as.numeric(as.character(qda_preds)), rf = as.numeric(as.character(rf_preds)), loess = as.numeric(as.character(loess_preds)), knn = as.numeric(as.character(knn_preds)))
 
@@ -165,7 +152,6 @@ ensemble_preds  <- apply(ensemble[,-1], 1, function(idx) {
 })
 sapply(ensemble_preds, paste, sep="", collapse = "")
 
-#ensemble_preds <- replace(ensemble_preds, ensemble_preds==1, "Kama") %>% replace( ensemble_preds==2, "Rosa") %>% replace(ensemble_preds==3, "Canadian")
 
 accuracy <- mean(na.omit(as.numeric(as.character(ensemble_preds)) == test_y))
 accuracy
